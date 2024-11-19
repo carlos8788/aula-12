a1334805f893a809623a60366c439aa298a4ea89
Luis <100473510+carlos8788@users.noreply.github.com>
2024-11-19 08:50:28 -0300

    JS-1

---
+++ clase-13-JS/main.js
@@ -0,0 +1,88 @@
+
+let nombreAula = 'aula-12'
+let horarioAula = 'lunes 18h'
+let cantidadAlumnos = 33
+
+let unString = ''
+let segundoString = ""
+let tercerString = ``
+
+// Comentarios de una linea
+/**Comentario multilinea */
+
+let numero1 = 1
+let numero2 = 2
+
+
+// let nombre = 'Juan'
+// let apellido = 'Soriano'
+// let edad = 10
+
+// let nombreCompleto = `${nombre} ${apellido} -> Edad: ${edad}`
+
+// Booleanos 
+// Existen 2 posibildades true o false
+
+let booleano1 = 5 < 10
+
+console.log(booleano1)
+
+let booleano2 = 'hola' === 'hola'
+
+console.log(booleano2)
+
+
+// ENTRADA
+/**
+ * Variables o constantes
+ * Entrada de valores por medio de HTML o prompt
+ */
+
+// PROCESO:
+/**
+ * Cálculo o conversión de un string
+ */
+
+// Salida
+// Salida de información por consola u otros métodos
+
+
+// Entrada 
+let nombre = 'Juan'
+let apellido = 'Soriano'
+let edad = 10
+
+// Proceso
+let nombreCompleto = `Nombre completo: ${nombre} ${apellido} -> Edad: ${edad}`
+
+// Salida
+console.log(nombreCompleto)
+// alert(nombreCompleto)
+
+
+// constantes
+
+nombre = 'Tomás'
+
+const NOMBRE = 'Juan'
+
+nombreCompleto = `Nombre completo: ${NOMBRE} ${apellido} -> Edad: ${edad}`
+console.log(nombreCompleto)
+
+// Nombre completo
+const NOMBRE_COMPLETO = 'Javier Olmos'
+// num afiliado
+const NUM_AFILIADO = 11359
+// categoría
+let categoria = 'C'
+// club
+let club = 'Nueva Alianza'
+// DNI
+const DNI = "2813355713"
+// fecha Nac
+const FECHA_NACIMIENTO = '09/07/1999'
+// fecha Em
+const FECHA_EMISION = '30/08/2022'
+
+let resultado = 10 + 20
+console.log(resultado)
